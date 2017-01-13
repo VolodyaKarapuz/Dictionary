de5a839a534a448786f3bfedeb848cd02973626c
VolodyaKarapuz <VolodyaKarapuz42@gmail.com>
2017-01-13 21:10:41 +0300

    Работа со строками vol.4

--- Test3.cpp
+++ Test3.cpp
@@ -19,36 +19,27 @@ int main (int argc, char* argv[])
 	FILE *in, *out, *in2;
 	char str[50], str2[50];
 	char *n, *n1;
-	int fl;
 	in = fopen ("in.txt","r+");
 	out = fopen ("out.txt","r+");
 	in2 = fopen ("in2.txt", "r+");
 
-	while (!feof(in))
-	{	
-		if (fl==1) {fprintf(in2,"%s%s",str,".result\n"); 
+	while (!feof(in)){	
 		for (int i=0; i<50; i++)
 		{
 			str[i]=NULL;
 			str2[i]=NULL;
 		}
-		
+		int fl;
+		fl=0;
 
 		fscanf(in,"%s",str);
-		while (!feof(out))
-		{	
-			
-			fl=0;
-			for (int i=0; i<50; i++)
-			{
-				str2[i]=NULL;
-			}		
-			fscanf(out,"%s",str2);
-			for (int i=0; i<50; i++) if (str[i]!=str2[i]) fl=1;
-			if (fl==0)
-				{fprintf(in2,"%s%s",str,"\n"); break;}
-	
-		}
+		fscanf(out,"%s",str2);
+		for (int i=0; i<50; i++) if (str[i]!=str2[i]) fl=1;
+		if (fl==0)
+			{fprintf(in2,"%s%s",str,"\n");}
+			else
+			{fprintf(in2,"%s%s",str,".result\n");}
+		
 	}
 	
 	fclose(in);
