703fda0e995447b5c46fd702bce1df62f456d8de
VolodyaKarapuz <VolodyaKarapuz42@gmail.com>
2017-01-13 21:30:39 +0300

    Первые пробы с потоками

---
+++ Potoki.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cmath>
+#include <string.h>
+#include <stdio.h>
+#include <conio.h>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+
+
+
+int main (int argc, char* argv[])
+{
+	mutex mtx;
+	
+	FILE *in, *out, *in2, *in3, *in4;
+	char str[50], str2[50];
+	char *n, *n1;
+	int fl=0;
+	in = fopen ("in.txt","r+");
+	out = fopen ("out.txt","r+");
+	in2 = fopen ("in2.txt", "r+");
+	in3 = fopen ("in3.txt", "r+");
+	in4 = fopen ("in4.txt", "r+");
+
+
+	thread t1 (foo, ref(mtx), ref(in), ref(out), ref(in2));
+	thread t2 (bar, ref(mtx), ref(in3), ref(out), ref(in4));
+
+	t1.join();
+	t2.join();
+	fclose(in);
+	fclose(in2);
+	fclose(out);
+	fclose(in3);
+	fclose(in4);
+}
+
+
+
+
+void bar (mutex& mtx, FILE* in2, FILE* out, FILE *in)
+{
+	char str[50], str2[50];
+	int fl=0;
+
+	while (!feof(in))
+	{
+		if (fl==1) fprintf(in2,"%s%s",str,".result\n");
+		for (int i=0; i<50; i++)
+		{
+			str[i]=NULL;
+			str2[i]=NULL;
+		}
+
+		fscanf(in,"%s",str);
+		mtx.lock();
+		out = fopen ("out.txt","r+");
+		while (!feof(out))
+		{	
+
+			fl=0;
+			for (int i=0; i<50; i++)
+			{
+				str2[i]=NULL;
+			}		
+			fscanf(out,"%s",str2);
+			for (int i=0; i<50; i++) if (str[i]!=str2[i]) fl=1;
+			if (fl==0)
+			{fprintf(in2,"%s%s",str,"\n"); break;}
+		}
+		fclose(out);
+		mtx.unlock();
+		usleep(50000);
+	}
+}
+
+void foo (mutex& mtx, FILE *in3, FILE *out, FILE *in4)
+{
+	
+	char str[50], str2[50];
+	int fl=0;
+
+	while (!feof(in3))
+	{
+		if (fl==1) fprintf(in4,"%s%s",str,".result\n");
+		for (int i=0; i<50; i++)
+		{
+			str[i]=NULL;
+			str2[i]=NULL;
+		}
+
+		fscanf(in3,"%s",str);
+		mtx.lock();
+		out = fopen ("out.txt","r+");
+		while (!feof(out))
+		{	
+
+			fl=0;
+			for (int i=0; i<50; i++)
+			{
+				str2[i]=NULL;
+			}		
+			fscanf(out,"%s",str2);
+			for (int i=0; i<50; i++) if (str[i]!=str2[i]) fl=1;
+			if (fl==0)
+			{fprintf(in4,"%s%s",str,"\n"); break;}
+		}
+		fclose(out);
+		mtx.unlock();
+		usleep(50000);
+	}			
+	
+}
